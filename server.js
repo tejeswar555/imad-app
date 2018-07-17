8b1fb80eac580ef19db2b923a7f4f8081cf06931
Tejeswara Reddy Buchupalli <35872097+tejeswar555@users.noreply.github.com>
2018-07-17 20:21:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/article-one', function (req,res) {
-    res.send('article one requested');
-});
     
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
