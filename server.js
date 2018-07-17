7ad4cc84459b4662e1d8fe43d60a6649b840fa8d
Tejeswara Reddy Buchupalli <35872097+tejeswar555@users.noreply.github.com>
2018-07-17 21:03:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/articleone', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'articleone.html*'));
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html*'));
 });
 
     
